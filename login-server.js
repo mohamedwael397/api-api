38e204a4238cef23212ebbdb5888fca7395be91c
mohamedwael397 <amedwael849@gmail.com>
2025-08-21 11:54:29 +0300

    Fix: use process.env.PORT

--- login-server.js
+++ login-server.js
@@ -198,7 +198,8 @@ app.get('/products', (req, res) => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
